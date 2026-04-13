d531e94048986cd51ca51b3017270744533be78f
diegonatylab <diegonatylab@gmail.com>
2026-04-13 11:40:30 -0300

    Update interest and cumulative interest styling

--- src/components/AmortizationTable.tsx
+++ src/components/AmortizationTable.tsx
@@ -37,10 +37,10 @@ export default function AmortizationTable({ schedule, currency }: Props) {
               <td class="px-3 py-2 text-right whitespace-nowrap">{row.date}</td>
               <td class="px-3 py-2 text-right">{fmt(row.payment)}</td>
               <td class="px-3 py-2 text-right text-blue-700 dark:text-blue-400">{fmt(row.principalPart)}</td>
-              <td class="px-3 py-2 text-right text-red-600 dark:text-red-400">{fmt(row.interestPart)}</td>
+              <td class="px-3 py-2 text-right text-red-700 dark:text-red-400">{fmt(row.interestPart)}</td>
               <td class="px-3 py-2 text-right">{fmt(row.balance)}</td>
-              <td class="px-3 py-2 text-right text-red-500 dark:text-red-400">{fmt(row.cumulativeInterest)}</td>
-              <td class="px-3 py-2 text-right text-blue-500 dark:text-blue-400">{fmt(row.cumulativePrincipal)}</td>
+              <td class="px-3 py-2 text-right text-red-700 dark:text-red-400">{fmt(row.cumulativeInterest)}</td>
+              <td class="px-3 py-2 text-right text-blue-700 dark:text-blue-400">{fmt(row.cumulativePrincipal)}</td>
             </tr>
           ))}
         </tbody>
